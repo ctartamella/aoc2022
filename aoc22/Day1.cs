26a74764838be4eb81998a0ecc2fbff8d649e73a
Chris Tartamella <ctartamella@softerware.com>
2022-12-02 13:46:19 -0700

    Cleanup and comments.

--- aoc22/Day1.cs
+++ aoc22/Day1.cs
@@ -11,6 +11,10 @@ public static class Day1
     
     public static int PartA()
     {
+        // Split the file into groups by double new lines (blank line) -> array[string], sizeof(NElves)
+        // Convert to array[array[string]] by reading the lines from each group. sizeof(NElves) x sizeof(NItemsForElf), sum(NItemsForElf)=NLines
+        // For each element, return the same list with strings converted to ints.  Where that fails, 0 is fine since we are summing.
+        // Take the max for each elf, computing the sum of each elves list as we go.
         return File
             .Split("\n\n")
             .Select(x => x.Split('\n'))
@@ -20,6 +24,7 @@ public static class Day1
 
     public static int PartB()
     {
+        // Same as before, but generate the full list of sums, sort them, then take the top 3, then sum them.
         return File
             .Split("\n\n")
             .Select(x => x.Split('\n'))
--- aoc22/Day2.cs
+++ aoc22/Day2.cs
@@ -2,52 +2,44 @@ namespace aoc22;
 
 public static class Day2
 {
+    // Enums internally _are_ ints, so we can effectively map the char values to int for indices.
     private enum Them { A, B, C }
     private enum Us { X, Y, Z }
-    private enum Outcome { L, D, W }
     
     private static readonly IEnumerable<string> File;
 
-    private static readonly int[,] Results;
-    private static readonly int[,] Results2;
+    // List of RPS outcomes and score values for us.  Them x Us for the axes. Value is the score.
+    private static readonly int[,] Results = {{4, 8, 3}, {1, 5, 9}, {7, 2, 6}};
+    
+    // Given their play and the desired result, what should we play?  Them x Desired for the axes here. Value is our play.
+    private static readonly int[,] Results2 = {{2,0,1}, {0,1,2}, {1,2,0}};
     
     static Day2()
     {
         File = System.IO.File.ReadLines("2.txt");
-        Results = new[,] {{4, 8, 3}, {1, 5, 9}, {7, 2, 6}};
-        Results2 = new[,] {{2,0,1}, {0,1,2}, {1,2,0}};
     }
     
     public static int PartA()
     {
-        int sum = 0;
-        foreach (var line in File)
-        {
-            var play = line.Split(' ');
-            var them = (int) Enum.Parse<Them>(play[0]);
-            var us = (int) Enum.Parse<Us>(play[1]);
-            var result = Results[them, us];
-            
-            sum += result;
-        }
+        var scores =
+            from line in File
+            select line.Split(' ') into play
+            let them = (int) Enum.Parse<Them>(play[0])
+            let us = (int) Enum.Parse<Us>(play[1])
+            select Results[them, us];
 
-        return sum;
+        return scores.Sum();
     }
     
     public static int PartB()
     {
-        int sum = 0;
-        foreach (var line in File)
-        {
-            var data = line.Split(' ');
-            var them = (int) Enum.Parse<Them>(data[0]);
-            var play = (int) Enum.Parse<Us>(data[1]);
-            var us = Results2[them, play];
-            var result = Results[them, us];
-            
-            sum += result;
-        }
+        var scores = from line in File
+            select line.Split(' ') into data
+            let them = (int) Enum.Parse<Them>(data[0])
+            let play = (int) Enum.Parse<Us>(data[1])
+            let us = Results2[them, play]
+            select Results[them, us];
 
-        return sum;
+        return scores.Sum();
     }
 }
