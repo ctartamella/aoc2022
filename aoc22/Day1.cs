dd57679fcff895c71294ef529af03522ff7f8d3b
Chris Tartamella <chris@tartamella.me>
2022-12-04 00:14:35 -0700

    Debugged a problem with line endings changing which broke a solution

--- aoc22/Day1.cs
+++ aoc22/Day1.cs
@@ -1,5 +1,8 @@
 namespace aoc22;
 
+// One thing i don't like.... this assumes certain line endings as I discovered when commiting on my 
+// mac then pulling on windows later.  Change between \r\n on Windows and \n on Unix machines.
+
 public static class Day1
 {
     private static readonly string File;
@@ -16,8 +19,9 @@ public static class Day1
         // For each element, return the same list with strings converted to ints.  Where that fails, 0 is fine since we are summing.
         // Take the max for each elf, computing the sum of each elves list as we go.
         return File
-            .Split("\n\n")
-            .Select(x => x.Split('\n'))
+            .Split("\r\n\r\n")
+            .Select(x => x.Split("\r\n")
+            .Select(x => x.Trim()))
             .Select(x => x.Select(y => !string.IsNullOrWhiteSpace(y) ? Convert.ToInt32(y) : 0))
             .Max(x => x.Sum());
     }
@@ -26,8 +30,8 @@ public static class Day1
     {
         // Same as before, but generate the full list of sums, sort them, then take the top 3, then sum them.
         return File
-            .Split("\n\n")
-            .Select(x => x.Split('\n'))
+            .Split("\r\n\r\n")
+            .Select(x => x.Split("\r\n"))
             .Select(x => x.Select(y => !string.IsNullOrWhiteSpace(y) ? Convert.ToInt32(y) : 0))
             .Select(x => x.Sum())
             .OrderByDescending(x => x)
